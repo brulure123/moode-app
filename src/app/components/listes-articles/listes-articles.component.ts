8aeda5777f82f2d8e30fbe1c02797783ce163f54
brulure123 <benraj.200@gmail.com>
2020-11-12 08:19:12 +0100

    Design de la page d'authentification et d'enregistrement.

---
+++ src/app/components/listes-articles/listes-articles.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listes-articles',
+  templateUrl: './listes-articles.component.html',
+  styleUrls: ['./listes-articles.component.scss'],
+})
+export class ListesArticlesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
